372de4caf1f2657ba962f8f946e39b410cbc2fb3
xdrelos <pierrejeancedric@gmail.com>
2021-04-10 11:54:35 +0200

    add association category to create and edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.paginate(page: params[:page], per_page: 5).order("created_at DESC")
   end
 
   def new
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title,:description)
+    params.require(:article).permit(:title,:description, category_ids: [])
   end
 
   def require_same_user
